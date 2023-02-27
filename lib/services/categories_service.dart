8463cd906fd734d38b1ae63f00e8d7fa69d2a68c
github_username <you@domain.com>
2023-02-27 10:40:41 +0200

    commit

--- lib/services/categories_service.dart
+++ lib/services/categories_service.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 class CategoriesService {
   Future<List<ProductModel>> getCategoriesProducts(
       {required String categoryName}) async {
-  List<dynamic> data   =  await Api().get(url: 'https://fakestoreapi.com/products/category/$categoryName');
+  List<dynamic> data   =  await Api().get(url: 'https://fakestoreapi.com/products/category/$categoryName', token: '');
 
       List<ProductModel> productsList = [];
       for (int i = 0; i < data.length; i++) {
--- lib/services/get_all_product_service.dart
+++ lib/services/get_all_product_service.dart
@@ -7,7 +7,7 @@ import 'package:store_app/models/product_model.dart';
 class AllProductsService {
   Future<List<ProductModel>> getAllProducts() async {
     List<dynamic> data =
-        await Api().get(url: 'https://fakestoreapi.com/products');
+        await Api().get(url: 'https://fakestoreapi.com/products', token: '');
 
     List<ProductModel> productsList = [];
     for (int i = 0; i < data.length; i++) {
